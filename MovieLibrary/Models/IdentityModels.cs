de8ea8da99fc2072d17b9c9259a9eb361574bd60
tnayanam <nayatanuj@gmail.com>
2017-04-15 18:00:54 -0400

    Adding membership type to DB

--- MovieLibrary/Models/IdentityModels.cs
+++ MovieLibrary/Models/IdentityModels.cs
@@ -1,8 +1,8 @@
-﻿using System.Data.Entity;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace MovieLibrary.Models
 {
@@ -20,6 +20,9 @@ namespace MovieLibrary.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+
+        public DbSet<Customer> Customers { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
