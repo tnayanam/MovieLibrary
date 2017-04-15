480145a4a46dc93dddeb53b9ba8db7ca36454790
tnayanam <nayatanuj@gmail.com>
2017-04-15 14:19:10 -0400

    Pass a model to a partial view, add CSS class dynamically as per the model logic

---
+++ MovieLibrary/Controllers/MoviesController.cs
@@ -0,0 +1,39 @@
+﻿using MovieLibrary.Models;
+using MovieLibrary.ViewModel;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MovieLibrary.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie
+            {
+                Name = "Shrek"
+            };
+
+            var customers = new List<Customer>
+            {
+                new Customer
+                {
+                    Name = "Tanuj"
+                },
+                new Customer
+                {
+                    Name ="Nayanam"
+                }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Customers = customers,
+                Movie = movie
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ MovieLibrary/ViewModel/RandomMovieViewModel.cs
@@ -0,0 +1,11 @@
+﻿using MovieLibrary.Models;
+using System.Collections.Generic;
+
+namespace MovieLibrary.ViewModel
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
