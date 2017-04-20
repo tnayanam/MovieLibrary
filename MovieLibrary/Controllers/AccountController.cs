50a2803dce32382eb664e0d69b47d2d812b87067
tnayanam <nayatanuj@gmail.com>
2017-04-19 23:47:14 -0400

    Adding fields in registration form

--- MovieLibrary/Controllers/AccountController.cs
+++ MovieLibrary/Controllers/AccountController.cs
@@ -148,7 +148,7 @@ namespace MovieLibrary.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, DrivingLicense = model.DrivingLicense };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
---
+++ MovieLibrary/Migrations/201704200341298_profiledata.cs
@@ -0,0 +1,18 @@
+namespace MovieLibrary.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class profiledata : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "DrivingLicense", c => c.String(nullable: false, maxLength: 255));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "DrivingLicense");
+        }
+    }
+}
