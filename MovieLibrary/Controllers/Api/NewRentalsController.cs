87516642d1bca90a157af22dbe0dc1ed8c6255d8
tnayanam <nayatanuj@gmail.com>
2017-04-25 23:23:42 -0400

    Adding API, rental class and the dto for renting the movie

---
+++ MovieLibrary/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,27 @@
+﻿using MovieLibrary.Dtos;
+using MovieLibrary.Models;
+using System;
+using System.Linq;
+using System.Web.Http;
+
+namespace MovieLibrary.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public NewRentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
+        {
+            var customer = _context.Customers.Single(c => c.Id == newRental.CustomerId);
+
+            var movies = _context.Movies.Where(m => newRental.MovieIds.Contains(m.Id));
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MovieLibrary/Dtos/NewRentalDto.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace MovieLibrary.Dtos
+{
+    public class NewRentalDto
+    {
+        public int CustomerId { get; set; }
+        public List<int> MovieIds { get; set; }
+    }
+}
