f0162b9afe37292da03f01685801c65fb3ac97cf
tnayanam <nayatanuj@gmail.com>
2017-05-07 23:18:13 -0400

    not found 404 error handling by MVC framework, this cannot be handled by HandleErrorAttribute

--- MovieLibrary/App_Start/FilterConfig.cs
+++ MovieLibrary/App_Start/FilterConfig.cs
@@ -6,7 +6,7 @@ namespace MovieLibrary
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new HandleErrorAttribute()); // handled any unhandled exception in any of the controller acftiuon and dispalys the custome rerror message
             filters.Add(new AuthorizeAttribute());
             filters.Add(new RequireHttpsAttribute());
         }
--- MovieLibrary/Controllers/HomeController.cs
+++ MovieLibrary/Controllers/HomeController.cs
@@ -22,6 +22,7 @@ namespace MovieLibrary.Controllers
 
         public ActionResult Contact()
         {
+            throw new HttpAntiForgeryException();
             ViewBag.Message = "Your contact page.";
 
             return View();
